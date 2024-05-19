332d52a155de5b731a0d697cea89fa6bf4538cfa
mulqY1122 <mulkipadilah0101@gmail.com>
2024-05-19 22:38:33 +0700

    Update add-teacher.blade.php
    ini bagian add

--- resources/views/teacher/add-teacher.blade.php
+++ resources/views/teacher/add-teacher.blade.php
@@ -1,17 +1,16 @@
-
 @extends('layouts.master')
 @section('content')
-{{-- message --}}
+{{-- pesan --}}
 {!! Toastr::message() !!}
 <div class="page-wrapper">
     <div class="content container-fluid">
         <div class="page-header">
             <div class="row align-items-center">
                 <div class="col">
-                    <h3 class="page-title">Add Teachers</h3>
+                    <h3 class="page-title">Tambahkan Guru</h3>
                     <ul class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="teachers.html">Teachers</a></li>
-                        <li class="breadcrumb-item active">Add Teachers</li>
+                        <li class="breadcrumb-item"><a href="teachers.html">Guru</a></li>
+                        <li class="breadcrumb-item active">Tambahkan Guru</li>
                     </ul>
                 </div>
             </div>
@@ -25,13 +24,13 @@
                             @csrf
                             <div class="row">
                                 <div class="col-12">
-                                    <h5 class="form-title"><span>Basic Details</span></h5>
+                                    <h5 class="form-title"><span>Detail Dasar</span></h5>
                                 </div>
                                 <div class="col-12 col-sm-4">
                                     <div class="form-group local-forms">
-                                        <label>Full Name<span class="login-danger">*</span></label>
+                                        <label>Nama Lengkap<span class="login-danger">*</span></label>
                                         <select class="select select2s-hidden-accessible @error('full_name') is-invalid @enderror" style="width: 100%;" tabindex="-1" aria-hidden="true" id="full_name" name="full_name">
-                                            <option selected disabled>-- Select Name --</option>
+                                            <option selected disabled>-- Pilih Nama --</option>
                                             @foreach($users as $key => $names)
                                                 <option value="{{ $names->name }}"data-teacher_id={{ $names->user_id }} {{ old('full_name') == $names->name ? "selected" :""}}>{{ $names->name }}</option>
                                             @endforeach
@@ -45,18 +44,18 @@
                                 </div>
                                 <div class="col-12 col-sm-4">
                                     <div class="form-group local-forms">
-                                        <label>Teacher ID <span class="login-danger">*</span></label>
-                                        <input type="text" class="form-control" name="teacher_id" id="teacher_id" placeholder="Teacher ID" value="{{ old('teacher_id') }}" readonly>
+                                        <label>ID Guru <span class="login-danger">*</span></label>
+                                        <input type="text" class="form-control" name="teacher_id" id="teacher_id" placeholder="ID Guru" value="{{ old('teacher_id') }}" readonly>
                                     </div>
                                 </div>
                                 <div class="col-12 col-sm-4">
                                     <div class="form-group local-forms">
-                                        <label>Gender <span class="login-danger">*</span></label>
+                                        <label>Jenis Kelamin <span class="login-danger">*</span></label>
                                         <select class="form-control select  @error('gender') is-invalid @enderror" name="gender">
-                                            <option selected disabled>Select Gender</option>
-                                            <option value="Female" {{ old('gender') == 'Female' ? "selected" :"Female"}}>Female</option>
-                                            <option value="Male" {{ old('gender') == 'Male' ? "selected" :""}}>Male</option>
-                                            <option value="Others" {{ old('gender') == 'Others' ? "selected" :""}}>Others</option>
+                                            <option selected disabled>Pilih Jenis Kelamin</option>
+                                            <option value="Female" {{ old('gender') == 'Female' ? "selected" :"Female"}}>Perempuan</option>
+                                            <option value="Male" {{ old('gender') == 'Male' ? "selected" :""}}>Laki-laki</option>
+                                            <option value="Others" {{ old('gender') == 'Others' ? "selected" :""}}>Lainnya</option>
                                         </select>
                                         @error('gender')
                                             <span class="invalid-feedback" role="alert">
@@ -67,8 +66,8 @@
                                 </div>
                                 <div class="col-12 col-sm-4">
                                     <div class="form-group local-forms">
-                                        <label>Experience <span class="login-danger">*</span></label>
-                                        <input type="text" class="form-control @error('experience') is-invalid @enderror" name="experience" placeholder="Enter Experience" value="{{ old('experience') }}">
+                                        <label>Pengalaman <span class="login-danger">*</span></label>
+                                        <input type="text" class="form-control @error('experience') is-invalid @enderror" name="experience" placeholder="Masukkan Pengalaman" value="{{ old('experience') }}">
                                         @error('experience')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -78,7 +77,7 @@
                                 </div>
                                 <div class="col-12 col-sm-4">
                                     <div class="form-group local-forms calendar-icon">
-                                        <label>Qualification <span class="login-danger">*</span></label>
+                                        <label>Kualifikasi <span class="login-danger">*</span></label>
                                         <input type="text" class="form-control datetimepicker @error('qualification') is-invalid @enderror" name="qualification" placeholder="DD-MM-YYYY" value="{{ old('qualification') }}">
                                         @error('qualification')
                                             <span class="invalid-feedback" role="alert">
@@ -89,7 +88,7 @@
                                 </div>
                                 <div class="col-12 col-sm-4">
                                     <div class="form-group local-forms calendar-icon">
-                                        <label>Date Of Birth <span class="login-danger">*</span></label>
+                                        <label>Tanggal Lahir <span class="login-danger">*</span></label>
                                         <input type="text" class="form-control datetimepicker @error('date_of_birth') is-invalid @enderror" name="date_of_birth" placeholder="DD-MM-YYYY" value="{{ old('date_of_birth') }}">
                                         @error('date_of_birth')
                                             <span class="invalid-feedback" role="alert">
@@ -100,12 +99,12 @@
                                 </div>
 
                                 <div class="col-12">
-                                    <h5 class="form-title"><span>Address</span></h5>
+                                    <h5 class="form-title"><span>Alamat</span></h5>
                                 </div>
                                 <div class="col-12 col-sm-6">
                                     <div class="form-group local-forms">
-                                        <label>Address <span class="login-danger">*</span></label>
-                                        <input type="text" class="form-control @error('address') is-invalid @enderror" name="address" placeholder="Enter address" value="{{ old('address') }}">
+                                        <label>Alamat <span class="login-danger">*</span></label>
+                                        <input type="text" class="form-control @error('address') is-invalid @enderror" name="address" placeholder="Masukkan alamat" value="{{ old('address') }}">
                                         @error('address')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -115,8 +114,8 @@
                                 </div>
                                 <div class="col-12 col-sm-6">
                                     <div class="form-group local-forms">
-                                        <label>Phone </label>
-                                        <input class="form-control @error('phone_number') is-invalid @enderror" type="text" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1').replace(/^0[^.]/, '0');" name="phone_number" placeholder="Enter Phone Number" value="{{ old('phone_number') }}">
+                                        <label>Nomor Telepon </label>
+                                        <input class="form-control @error('phone_number') is-invalid @enderror" type="text" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1').replace(/^0[^.]/, '0');" name="phone_number" placeholder="Masukkan Nomor Telepon" value="{{ old('phone_number') }}">
                                         @error('phone_number')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -126,8 +125,8 @@
                                 </div>
                                 <div class="col-12 col-sm-4">
                                     <div class="form-group local-forms">
-                                        <label>City <span class="login-danger">*</span></label>
-                                        <input type="text" class="form-control @error('city') is-invalid @enderror" name="city" placeholder="Enter City" value="{{ old('city') }}">
+                                        <label>Kota <span class="login-danger">*</span></label>
+                                        <input type="text" class="form-control @error('city') is-invalid @enderror" name="city" placeholder="Masukkan Kota" value="{{ old('city') }}">
                                         @error('city')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -137,8 +136,8 @@
                                 </div>
                                 <div class="col-12 col-sm-4">
                                     <div class="form-group local-forms">
-                                        <label>State <span class="login-danger">*</span></label>
-                                        <input type="text" class="form-control @error('state') is-invalid @enderror" name="state" placeholder="Enter State" value="{{ old('state') }}">
+                                        <label>Provinsi <span class="login-danger">*</span></label>
+                                        <input type="text" class="form-control @error('state') is-invalid @enderror" name="state" placeholder="Masukkan Provinsi" value="{{ old('state') }}">
                                         @error('state')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -148,8 +147,8 @@
                                 </div>
                                 <div class="col-12 col-sm-4">
                                     <div class="form-group local-forms">
-                                        <label>Zip Code <span class="login-danger">*</span></label>
-                                        <input type="text" class="form-control @error('zip_code') is-invalid @enderror" name="zip_code" placeholder="Enter Zip" value="{{ old('zip_code') }}">
+                                        <label>Kode Pos <span class="login-danger">*</span></label>
+                                        <input type="text" class="form-control @error('zip_code') is-invalid @enderror" name="zip_code" placeholder="Masukkan Kode Pos" value="{{ old('zip_code') }}">
                                         @error('zip_code')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -159,8 +158,8 @@
                                 </div>
                                 <div class="col-12 col-sm-4">
                                     <div class="form-group local-forms">
-                                        <label>Country <span class="login-danger">*</span></label>
-                                        <input type="text" class="form-control @error('country') is-invalid @enderror" name="country" placeholder="Enter Country" value="{{ old('country') }}">
+                                        <label>Negara <span class="login-danger">*</span></label>
+                                        <input type="text" class="form-control @error('country') is-invalid @enderror" name="country" placeholder="Masukkan Negara" value="{{ old('country') }}">
                                         @error('country')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -170,7 +169,7 @@
                                 </div>
                                 <div class="col-12">
                                     <div class="student-submit">
-                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                        <button type="submit" class="btn btn-primary">Kirim</button>
                                     </div>
                                 </div>
                             </div>
@@ -183,7 +182,7 @@
 </div>
 @section('script')
 <script>
-    // select auto teacher id
+    // pilih otomatis id guru
     $('#full_name').on('change',function()
     {
         $('#teacher_id').val($(this).find(':selected').data('teacher_id'));
