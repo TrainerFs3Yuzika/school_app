72952a4bd4a23fa9c208dd468edd17d7c646ca29
mulqY1122 <mulkipadilah0101@gmail.com>
2024-05-21 00:45:23 +0700

    Update home.blade.php

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -182,53 +182,59 @@
                     </div>
                 </div>
             </div>
-                        {{-- Data Kelas --}}
-                        <div class="row">
-                            <div class="col-xl-12">
-                                <div class="card shadow-lg">
-                                    <div class="card-header bg-warning text-white">
-                                        <h5 class="card-title mb-0">Data Kelas</h5>
-                                    </div>
-                                    <div class="card-body">
-                                        <div class="table-responsive">
-                                            <table class="table table-striped table-bordered table-hover table-center">
-                                                <thead class="thead-dark">
-                                                    <tr>
-                                                        <th>ID</th>
-                                                        <th>Nama Kelas</th>
-                                                        <th>Nama Guru</th>
-                                                        <th>Nama Siswa</th>
-                                                        <th>Nama Mata Pelajaran</th> <!-- Mengubah "Mata pelajaran" menjadi "Mata Pelajaran" -->
-                                                        <th>Aksi</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    @foreach(\App\Models\ClassModel::all() as $class)
-                                                        <tr>
-                                                            <td>{{ $class->id }}</td>
-                                                            <td>{{ $class->class_name }}</td>
-                                                            <td>{{ $class->teacher->full_name }}</td>
-                                                            <td>{{ $class->student->first_name }} {{ $class->student->last_name }}</td>
-                                                            <td>{{ $class->subject->class }}</td>
-                                                            <td>
-                                                                <a href="{{ route('classes.edit', $class->id) }}" class="btn btn-sm btn-info">Edit</a>
-                                                                <form action="{{ route('classes.destroy', $class->id) }}" method="POST" class="d-inline">
-                                                                    @csrf
-                                                                    @method('DELETE')
-                                                                    <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Anda yakin ingin menghapus kelas ini?')">Hapus</button>
-                                                                </form>
-                                                            </td>
-                                                        </tr>
-                                                    @endforeach
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
+
+            {{-- Data Kelas --}}
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="card shadow-lg" style="border-radius: 15px;">
+                        <div class="card-header bg-primary text-white">
+                            <h5 class="card-title mb-0">Data Kelas</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover table-center rounded" style="border-radius: 15px;">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nama Kelas</th>
+                                            <th>Nama Guru</th>
+                                            <th>Nama Siswa</th>
+                                            <th>Nama Mata Pelajaran</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach(\App\Models\ClassModel::all() as $class)
+                                            <tr>
+                                                <td>{{ $class->id }}</td>
+                                                <td>{{ $class->class_name }}</td>
+                                                <td>{{ $class->teacher->full_name }}</td>
+                                                <td>{{ $class->student->first_name }} {{ $class->student->last_name }}</td>
+                                                <td>{{ $class->subject->class }}</td>
+                                                <td>
+                                                    <a href="{{ route('classes.edit', $class->id) }}" class="btn btn-sm btn-warning edit-btn"><i class="fas fa-edit"></i> Edit</a>
+                                                    <form action="{{ route('classes.destroy', $class->id) }}" method="POST" class="d-inline delete-form">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="btn btn-sm btn-danger delete-btn"><i class="fas fa-trash-alt"></i> Hapus</button>
+                                                    </form>
+                                                </td>
+                                                
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
-                        <a href="{{ route('classes.create') }}" class="btn btn-primary mb-3">Tambah Kelas Baru</a>
-                        {{-- Data Kelas --}}
+                    </div>
+                </div>
+            </div>
+            <a href="{{ route('classes.create') }}" class="btn btn-primary mb-3">Tambah Kelas Baru</a>
+            {{-- Data Kelas --}}
+
+            <!--Tambahkan Script Font Awesome -->
+            <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
 
         <div class="row">
             <div class="col-xl-12">
