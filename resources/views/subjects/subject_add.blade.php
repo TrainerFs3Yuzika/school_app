6d406cdce13c71b1e948fde843bce03056be2dcf
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2024-05-23 21:32:34 +0800

    Update mata pelajaran

--- resources/views/subjects/subject_add.blade.php
+++ resources/views/subjects/subject_add.blade.php
@@ -1,17 +1,16 @@
-
 @extends('layouts.master')
 @section('content')
-    {{-- message --}}
+    {{-- pesan --}}
     {!! Toastr::message() !!}
     <div class="page-wrapper">
         <div class="content container-fluid">
             <div class="page-header">
                 <div class="row align-items-center">
                     <div class="col">
-                        <h3 class="page-title">Add Subject</h3>
+                        <h3 class="page-title">Tambah Mata Pelajaran</h3>
                         <ul class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="subjects.html">Subject</a></li>
-                            <li class="breadcrumb-item active">Add Subject</li>
+                            <li class="breadcrumb-item"><a href="subjects.html">Mata Pelajaran</a></li>
+                            <li class="breadcrumb-item active">Tambah Mata Pelajaran</li>
                         </ul>
                     </div>
                 </div>
@@ -25,23 +24,23 @@
                                 @csrf
                                 <div class="row">
                                     <div class="col-12">
-                                        <h5 class="form-title"><span>Subject Information</span></h5>
+                                        <h5 class="form-title"><span>Informasi Mata Pelajaran</span></h5>
                                     </div>
                                     <div class="col-12 col-sm-4">
                                         <div class="form-group local-forms">
-                                            <label>Subject Name <span class="login-danger">*</span></label>
-                                            <input type="text" class="form-control" name="subject_name" placeholder="Enter Subject Name">
+                                            <label>Nama Mata Pelajaran <span class="login-danger">*</span></label>
+                                            <input type="text" class="form-control" name="subject_name" placeholder="Masukkan Nama Mata Pelajaran">
                                         </div>
                                     </div>
                                     <div class="col-12 col-sm-4">
                                         <div class="form-group local-forms">
-                                            <label>Class <span class="login-danger">*</span></label>
-                                            <input type="text" class="form-control" name="class" placeholder="Enter Class">
+                                            <label>Kelas <span class="login-danger">*</span></label>
+                                            <input type="text" class="form-control" name="class" placeholder="Masukkan Kelas">
                                         </div>
                                     </div>
                                     <div class="col-12">
                                         <div class="student-submit">
-                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                            <button type="submit" class="btn btn-primary">Kirim</button>
                                         </div>
                                     </div>
                                 </div>
