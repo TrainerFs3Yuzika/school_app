7f7ceac75a0ee7bc37d7bc7f98306d176e481a85
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2024-05-27 21:21:09 +0800

    Update peminjaman form

--- resources/views/peminjaman/create.blade.php
+++ resources/views/peminjaman/create.blade.php
@@ -1,46 +1,71 @@
 <!-- resources/views/peminjaman/create.blade.php -->
-
-@extends('layouts.app')
-
+@extends('layouts.master')
 @section('content')
-    <div class="container">
-        <h1>Tambah Peminjaman Buku</h1>
-        <form action="{{ route('peminjaman.store') }}" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="book_id">Judul Buku:</label>
-                <select name="book_id" class="form-control" required>
-                    @foreach($books as $book)
-                        <option value="{{ $book->id }}">{{ $book->judul }}</option>
-                    @endforeach
-                </select>
+    <div class="page-wrapper">
+        <div class="content container-fluid">
+
+            <div class="page-header">
+                <div class="row align-items-center">
+                    <div class="col-sm-12">
+                        <div class="page-sub-header">
+                            <h3 class="page-title">Tambah Peminjaman</h3>
+                            <ul class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="{{ route('peminjaman.index') }}">Peminjaman</a></li>
+                                <li class="breadcrumb-item active">Tambah Peminjaman</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="nama_peminjam">Nama Peminjam:</label>
-                <input type="text" name="nama_peminjam" class="form-control" required>
+            {{-- pesan --}}
+            {!! Toastr::message() !!}
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card comman-shadow">
+                        <div class="card-body">
+                            <form action="{{ route('peminjaman.store') }}" method="POST">
+                                @csrf
+                                <div class="row">
+                                    <h5 class="form-title student-info">Informasi Peminjaman
+                                        <span>
+                                            <a href="javascript:;"><i class="feather-more-vertical"></i></a>
+                                        </span>
+                                    </h5>
+                                    <div class="col-md-6">
+                                        <div class="form-group local-forms">
+                                            <label for="book_id">Judul Buku:</label>
+                                            <select name="book_id" class="form-control" placeholder="Masukkan Nama Buku" required>
+                                                @foreach($books as $book)
+                                                    <option value="{{ $book->id }}">{{ $book->judul }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="form-group local-forms">
+                                            <label for="nama_peminjam">Nama Peminjam <span class="login-danger">*</span></label>
+                                            <input type="text" name="nama_peminjam" class="form-control" placeholder="Msukkan Nama Peminjaman" required>
+                                        </div>
+                                        <div class="form-group local-forms">
+                                            <label for="tanggal_pinjam">Tanggal Peminjaman <span class="login-danger">*</span></label>
+                                            <input type="date" name="tanggal_pinjam" class="form-control datetimepicker" placeholder="DD-MM-YYYY" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group local-forms">
+                                            <label for="tanggal_kembali">Tanggal Pengembalian <span class="login-danger">*</span></label>
+                                            <input type="date" name="tanggal_kembali" class="form-control datetimepicker" placeholder="DD-MM-YYYY" required>
+                                        </div>
+                                        <div class="form-group local-forms">
+                                            <label for="jumlah_buku">Jumlah Buku <span class="login-danger">*</span></label>
+                                            <input type="number" name="jumlah_buku" class="form-control" min="1" placeholder="Masukkan Jumlah Buku" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="tanggal_pinjam">Tanggal Pinjam:</label>
-                <input type="date" name="tanggal_pinjam" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="tanggal_kembali">Tanggal Kembali:</label>
-                <input type="date" name="tanggal_kembali" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="jumlah_buku">Jumlah Buku:</label>
-                <input type="number" name="jumlah_buku" class="form-control" min="1" required>
-            </div>
-            {{-- <div class="form-group">
-                <label for="status">Status:</label>
-                <select name="status" class="form-control" required>
-                    <option value="belum_dikembalikan">Belum Dikembalikan</option>
-                    <option value="sudah_dikembalikan">Sudah Dikembalikan</option>
-                    <option value="diterima">Diterima</option>
-                    <option value="ditolak">Ditolak</option>
-                </select>
-            </div> --}}
-            <button type="submit" class="btn btn-primary">Simpan</button>
-        </form>
+        </div>
     </div>
 @endsection
