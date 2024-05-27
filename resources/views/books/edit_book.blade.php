d5fcffaf8f288c4b7f98fa14d1b993e4b53d3788
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2024-05-27 22:01:51 +0800

    Update edit_book.blade.php

--- resources/views/books/edit_book.blade.php
+++ resources/views/books/edit_book.blade.php
@@ -17,6 +17,7 @@
                     </div>
                 </div>
             </div>
+
             {{-- pesan --}}
             {!! Toastr::message() !!}
             <div class="row">
@@ -28,31 +29,39 @@
                                 @method('PUT')
                                 <div class="form-group">
                                     <label for="judul">Judul:</label>
-                                    <input type="text" name="judul" class="form-control" value="{{ $book->judul }}" required>
+                                    <input type="text" name="judul" class="form-control" value="{{ $book->judul }}"
+                                        required>
                                 </div>
                                 <div class="form-group">
                                     <label for="penulis">Penulis:</label>
-                                    <input type="text" name="penulis" class="form-control" value="{{ $book->penulis }}" required>
+                                    <input type="text" name="penulis" class="form-control" value="{{ $book->penulis }}"
+                                        required>
                                 </div>
                                 <div class="form-group">
                                     <label for="penerbit">Penerbit:</label>
-                                    <input type="text" name="penerbit" class="form-control" value="{{ $book->penerbit }}" required>
+                                    <input type="text" name="penerbit" class="form-control" value="{{ $book->penerbit }}"
+                                        required>
                                 </div>
                                 <div class="form-group">
                                     <label for="tahun_terbit">Tahun Terbit:</label>
-                                    <input type="number" name="tahun_terbit" class="form-control" value="{{ $book->tahun_terbit }}" required>
+                                    <input type="number" name="tahun_terbit" class="form-control"
+                                        value="{{ $book->tahun_terbit }}" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="genre">Genre:</label>
                                     <select name="genre" class="form-control" required>
-                                        <option value="fiksi" {{ $book->genre == 'fiksi' ? 'selected' : '' }}>Fiksi</option>
-                                        <option value="non-fiksi" {{ $book->genre == 'non-fiksi' ? 'selected' : '' }}>Non-Fiksi</option>
-                                        <option value="pelajaran" {{ $book->genre == 'pelajaran' ? 'selected' : '' }}>Pelajaran</option>
+                                        <option value="fiksi" {{ $book->genre == 'fiksi' ? 'selected' : '' }}>Fiksi
+                                        </option>
+                                        <option value="non-fiksi" {{ $book->genre == 'non-fiksi' ? 'selected' : '' }}>
+                                            Non-Fiksi</option>
+                                        <option value="pelajaran" {{ $book->genre == 'pelajaran' ? 'selected' : '' }}>
+                                            Pelajaran</option>
                                     </select>
                                 </div>
                                 <div class="form-group">
                                     <label for="stok">Stok:</label>
-                                    <input type="number" name="stok" class="form-control" value="{{ $book->stok }}" required>
+                                    <input type="number" name="stok" class="form-control" value="{{ $book->stok }}"
+                                        required>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Simpan</button>
                             </form>
