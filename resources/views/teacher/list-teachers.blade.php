50ce02947d834d008331cf78efbb38e5353eaf0d
Doni Chandra <40625723+donichan07@users.noreply.github.com>
2024-06-11 00:49:15 +0700

    Update Tombol
    tombol

--- resources/views/teacher/list-teachers.blade.php
+++ resources/views/teacher/list-teachers.blade.php
@@ -30,13 +30,7 @@
                                     <h3 class="page-title">Daftar Guru</h3>
                                 </div>
                                 <div class="col-auto text-end float-end ms-auto download-grp">
-                                    <a href="teachers.html" class="btn btn-outline-gray me-2 active">
-                                        <i class="fa fa-list" aria-hidden="true"></i>
-                                    <a href="{{ route('teacher/grid/page') }}" class="btn btn-outline-gray me-2">
-                                        <i class="fa fa-th" aria-hidden="true"></i>
-                                    <a href="#" class="btn btn-outline-primary me-2"><i
-                                            class="fas fa-download"></i> Unduh</a>
-                                    <a href="{{ route('teacher/add/page') }}" class="btn btn-primary"><i class="fas fa-plus"></i></a>
+                                    <a href="{{ route('teacher/add/page') }}" class="btn btn-primary">Tambah Guru <i class="fas fa-plus"></i></a>
                                 </div>
                             </div>
                         </div>
