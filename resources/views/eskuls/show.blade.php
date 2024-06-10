374a3b35cae0b8a743ef9a836475f9fffd5a5b5f
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2024-06-10 21:50:45 +0800

    Update show.blade.php

--- resources/views/eskuls/show.blade.php
+++ resources/views/eskuls/show.blade.php
@@ -7,7 +7,7 @@
             <div class="row">
                 <div class="col-sm-12">
                     <div class="page-sub-header">
-                        <h3 class="page-title">Detail Eskul</h3>
+                        <h3 class="page-title">Detail</h3>
                     </div>
                 </div>
             </div>
@@ -17,11 +17,12 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Nama Eskul: {{ $eskul->nama_eskul }}</h5>
-                        <p class="card-text">Pembina: {{ $eskul->pembina }}</p>
-                        <p class="card-text">Waktu Eskul: {{ $eskul->waktu_eskul }}</p>
-                        <p class="card-text">Siswa: {{ $eskul->student->first_name }} {{ $eskul->student->last_name }}</p>
+                        <h5 class="card-title">Nama Siswa: {{ $eskul->student->first_name }} {{ $eskul->student->last_name }}</h5>
+                        <p class="card-text fw-bold">Pembina: {{ $eskul->pembina }}</p>
+                        <p class="card-text">Ekstrakurikuler: {{ $eskul->nama_eskul }}</p>
+                        <p class="card-text">Waktu : <b>{{ $eskul->waktu_eskul }}</b></p>
                         <img src="{{ asset('images/' . $eskul->gambar) }}" alt="Gambar Eskul" class="img-fluid mb-3" style="max-width: 300px;">
+                        <br>
                         <a href="{{ route('eskuls.index') }}" class="btn btn-primary">Kembali</a>
                     </div>
                 </div>
