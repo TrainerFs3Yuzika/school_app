1d165fb7079075a4810533a2ad8e8121d01cd039
Doni Chandra <40625723+donichan07@users.noreply.github.com>
2024-06-10 23:39:34 +0700

    Add Table Nilai
    Table Nilai

--- resources/views/dashboard/teacher_dashboard.blade.php
+++ resources/views/dashboard/teacher_dashboard.blade.php
@@ -209,66 +209,102 @@
                         <div class="col-12 col-lg-12 col-xl-12 d-flex">
                             <div class="card flex-fill comman-shadow">
                                 <div class="card-header d-flex align-items-center">
-                                    <h5 class="card-title">Riwayat Mengajar</h5>
-                                    <ul class="chart-list-out student-ellips">
-                                        <li class="star-menus"><a href="javascript:;"><i
-                                                    class="fas fa-ellipsis-v"></i></a></li>
-                                    </ul>
+                                    <h5 class="card-title">Nilai Siswa</h5>                                   
+                                    <div class="col-auto text-end float-end ms-auto download-grp">
+                                        @if (auth()->user()->role_name === 'Super Admin' || Session::get('role_name') === 'Teachers')
+                                        <a href="{{ route('scores.create') }}" class="btn btn-primary"><i
+                                                class="fas fa-plus"></i></a>
+                                        @endif
+                                    </div>
                                 </div>
-                                <div class="card-body">
-                                    <div class="teaching-card">
-                                        <ul class="steps-history">
-                                            <li>22 Sep</li>
-                                            <li>23 Sep</li>
-                                            <li>24 Sep</li>
-                                        </ul>
-                                        <ul class="activity-feed">
-                                            <li class="feed-item d-flex align-items-center">
-                                                <div class="dolor-activity">
-                                                    <span class="feed-text1"><a>Matematika</a></span>
-                                                    <ul class="teacher-date-list">
-                                                        <li><i class="fas fa-calendar-alt me-2"></i>5 September,
-                                                            2022</li>
-                                                        <li>|</li>
-                                                        <li><i class="fas fa-clock me-2"></i>09:00 - 10:00 pagi (60
-                                                            Menit)</li>
-                                                    </ul>
+                                <div class="table-responsive">
+                                <table id="studentTable"
+                                    class="table border-0 star-student table-hover table-center mb-0 table-striped">
+                                    <thead class="student-thread">
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Guru</th>
+                                            <th>Murid</th>
+                                            <th>Mata Pelajaran</th>
+                                            <th>Nilai</th>
+                                            @if (auth()->user()->role_name === 'Super Admin' || Session::get('role_name') === 'Teachers')
+                                            <th class="text-center">Aksi</th>
+                                            @endif
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach (\App\Models\Score::all() as $key => $score)
+                                            <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $score->teacher->full_name }}</td>
+                                                <td>{{ $score->student->first_name }} {{ $score->student->last_name }}</td>
+                                                <td>{{ $score->subject->subject_name }}</td>
+                                                @if ($score->score >= 80 && $score->score <= 100)
+                                                    <td><span
+                                                            class="bg-success opacity-50 px-2 py-1 rounded-pill text-white fw-bold">{{ $score->score }}</span>
+                                                    </td>
+                                                @elseif($score->score >= 70 && $score->score <= 79)
+                                                    <td><span
+                                                            class="bg-warning opacity-50 px-2 py-1 rounded-pill text-white fw-bold">{{ $score->score }}</span>
+                                                    </td>
+                                                @elseif($score->score >= 1 && $score->score <= 69)
+                                                    <td><span
+                                                            class="bg-danger opacity-50 px-2 py-1 rounded-pill text-white fw-bold">{{ $score->score }}</span>
+                                                    </td>
+                                                @endif
+                                                @if (auth()->user()->role_name === 'Super Admin' || Session::get('role_name') === 'Teachers')
+                                                <td class="text-center align-middle">
+                                                    <div class="actions d-flex justify-content-center align-items-center">
+                                                        <a href="{{ route('scores.show', $score->id) }}"
+                                                            class="btn btn-sm bg-danger-light me-2">
+                                                            <i class="far fa-eye me-2"></i>
+                                                        </a>
+                                                        <a href="{{ route('scores.edit', $score->id) }}"
+                                                            class="btn btn-sm bg-danger-light me-2">
+                                                            <i class="far fa-edit me-2"></i>
+                                                        </a>
+                                                        <button type="button"
+                                                            class="btn btn-sm bg-danger-light student_delete"
+                                                            data-bs-toggle="modal" data-bs-target="#deleteModal"
+                                                            data-id="{{ $score->id }}">
+                                                            <i class="far fa-trash-alt py-1"></i>
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                                @endif
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            {{-- model hapus siswa --}}
+                                <div class="modal custom-modal fade" id="deleteModal" role="dialog">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-body">
+                                                <div class="form-header">
+                                                    <h3>Hapus Nilai</h3>
+                                                    <p>Apakah Anda yakin ingin menghapus?</p>
                                                 </div>
-                                                <div class="activity-btns ms-auto">
-                                                    <button type="submit" class="btn btn-info">Sedang
-                                                        Berlangsung</button>
+                                                <div class="modal-btn delete-action">
+                                                    <form id="deleteForm" method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <input type="hidden" name="id" class="e_id" value="">
+                                                        <div class="row">
+                                                            <div class="col-6">
+                                                                <button type="submit" class="btn btn-primary continue-btn submit-btn"
+                                                                    style="border-radius: 5px !important;">Hapus</button>
+                                                            </div>
+                                                            <div class="col-6">
+                                                                <a href="#" data-bs-dismiss="modal"
+                                                                    class="btn btn-primary paid-cancel-btn">Batal</a>
+                                                            </div>
+                                                        </div>
+                                                    </form>
                                                 </div>
-                                            </li>
-                                            <li class="feed-item d-flex align-items-center">
-                                                <div class="dolor-activity">
-                                                    <span class="feed-text1"><a>Geografi </a></span>
-                                                    <ul class="teacher-date-list">
-                                                        <li><i class="fas fa-calendar-alt me-2"></i>5 September,
-                                                            2022</li>
-                                                        <li>|</li>
-                                                        <li><i class="fas fa-clock me-2"></i>09:00 - 10:00 pagi</li>
-                                                    </ul>
-                                                </div>
-                                                <div class="activity-btns ms-auto">
-                                                    <button type="submit" class="btn btn-info">Selesai</button>
-                                                </div>
-                                            </li>
-                                            <li class="feed-item d-flex align-items-center">
-                                                <div class="dolor-activity">
-                                                    <span class="feed-text1"><a>Botani</a></span>
-                                                    <ul class="teacher-date-list">
-                                                        <li><i class="fas fa-calendar-alt me-2"></i>5 September,
-                                                            2022</li>
-                                                        <li>|</li>
-                                                        <li><i class="fas fa-clock me-2"></i>09:00 - 10:00 pagi</li>
-                                                    </ul>
-                                                </div>
-                                                <div class="activity-btns ms-auto">
-                                                    <button type="submit" class="btn btn-info">Sedang
-                                                        Berlangsung</button>
-                                                </div>
-                                            </li>
-                                        </ul>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -325,4 +361,26 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('script')
+    {{-- Include DataTables JS --}}
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            
+
+            $(document).on('click', '.student_delete', function() {
+                var scoreId = $(this).data('id');
+                var url = '{{ route('scores.destroy', ':id') }}';
+                url = url.replace(':id', scoreId);
+                $('#deleteForm').attr('action', url);
+            });
+
+            @if (session('success'))
+                toastr.success('{{ session('success') }}');
+            @endif
+        });
+    </script>
 @endsection
